d555e214ff05becb7a2d194ca7b0d7218855c0f8
Clare06 <106062267+Clare06@users.noreply.github.com>
2023-08-21 16:43:29 +0530

    login init

--- chatFrontEnd/src/app/app.module.ts
+++ chatFrontEnd/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+import { Router, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule,
+    FormsModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ chatFrontEnd/src/app/endpoints/rest-endpoints.ts
@@ -0,0 +1,5 @@
+const root="http://localhost:8080";
+
+export const ENDPOINTS = {
+  LOGIN: `${root}/authenticate`,
+}
